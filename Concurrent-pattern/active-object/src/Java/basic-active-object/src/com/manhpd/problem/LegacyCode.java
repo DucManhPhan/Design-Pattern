0405241fff3d63a019fa0e19320883f03b91ee2b
ManhPD5 <ducmanhphan93@gmail.com>
2020-01-17 06:32:30 +0700

    udate files

---
+++ Concurrent-pattern/active-object/src/Java/basic-active-object/src/com/manhpd/problem/LegacyCode.java
@@ -0,0 +1,40 @@
+package com.manhpd.problem;
+
+public class LegacyCode {
+
+    private int x;
+
+    private int y;
+
+    public LegacyCode(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    private void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void m1() {
+        setX(9);
+        setY(0);
+    }
+
+    public void m2() {
+        setY(9);
+        setX(0);
+    }
+
+}
