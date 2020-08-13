eba6929213bc6afd59be8e0118cb0a97cb4e66ef
Le Thi Ha Trang <ducmanhphan93@gmail.com>
2020-08-13 13:16:45 +0700

    update files

---
+++ system-design/url-shortening-service/src/src/main/java/com/manhpd/url/shortening/demo/UrlShorteningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manhpd.url.shortening.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorteningServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShorteningServiceApplication.class, args);
+    }
+
+}
---
+++ system-design/url-shortening-service/src/src/test/java/com/manhpd/url/shortening/demo/UrlShorteningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhpd.url.shortening.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorteningServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
