f5f549313dfdd8b4b2dee5c658f9f8316808a084
Phan Manh <ducmanhphan93@gmail.com>
2020-11-05 20:48:57 +0700

    update files

---
+++ Structural-Pattern/Adapter-pattern/src/Python/basic-adapter/after/vend_adapter.py
@@ -0,0 +1,14 @@
+from abs_adapter import AbsAdapter
+
+
+class VendAdapter(AbsAdapter):
+    @property
+    def name(self):
+        return self.adaptee.name
+
+    @property
+    def address(self):
+        return '{} {}'.format(
+            self.adaptee.name,
+            self.adaptee.street
+        )
