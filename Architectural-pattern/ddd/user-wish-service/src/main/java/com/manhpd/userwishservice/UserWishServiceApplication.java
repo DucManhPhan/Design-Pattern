77d554b8eb9901dede1aed941abfe27e924c06a3
Phan Manh <ducmanhphan93@gmail.com>
2022-09-23 15:11:06 +0700

    add user-wish-service

---
+++ Architectural-pattern/ddd/user-wish-service/src/main/java/com/manhpd/userwishservice/UserWishServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manhpd.userwishservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWishServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserWishServiceApplication.class, args);
+    }
+
+}
---
+++ Architectural-pattern/ddd/user-wish-service/src/test/java/com/manhpd/userwishservice/UserWishServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhpd.userwishservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserWishServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
