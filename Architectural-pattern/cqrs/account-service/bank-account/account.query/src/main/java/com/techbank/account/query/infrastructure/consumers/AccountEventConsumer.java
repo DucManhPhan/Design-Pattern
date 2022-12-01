fe65d64a7f5952acd54fe693da91d5b85bc32897
Phan Manh <ducmanhphan93@gmail.com>
2022-12-01 17:20:28 +0700

    update bank accounts

---
+++ Architectural-pattern/cqrs/account-service/bank-account/account.query/src/main/java/com/techbank/account/query/infrastructure/consumers/AccountEventConsumer.java
@@ -0,0 +1,46 @@
+package com.techbank.account.query.infrastructure.consumers;
+
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawnEvent;
+import com.techbank.account.query.infrastructure.handlers.EventHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountEventConsumer implements EventConsumer {
+
+    @Autowired
+    private EventHandler eventHandler;
+
+    @KafkaListener(topics = "AccountOpenedEvent", groupId = "${spring.kafka.consumer.group-id}")
+    @Override
+    public void consume(AccountOpenedEvent event, Acknowledgment ack) {
+        this.eventHandler.on(event);
+        ack.acknowledge();
+    }
+
+    @KafkaListener(topics = "FundsDepositedEvent", groupId = "${spring.kafka.consumer.group-id}")
+    @Override
+    public void consume(FundsDepositedEvent event, Acknowledgment ack) {
+        this.eventHandler.on(event);
+        ack.acknowledge();
+    }
+
+    @KafkaListener(topics = "FundsWithdrawnEvent", groupId = "${spring.kafka.consumer.group-id}")
+    @Override
+    public void consume(FundsWithdrawnEvent event, Acknowledgment ack) {
+        this.eventHandler.on(event);
+        ack.acknowledge();
+    }
+
+    @KafkaListener(topics = "AccountClosedEvent", groupId = "${spring.kafka.consumer.group-id}")
+    @Override
+    public void consume(AccountClosedEvent event, Acknowledgment ack) {
+        this.eventHandler.on(event);
+        ack.acknowledge();
+    }
+}
---
+++ Architectural-pattern/cqrs/account-service/bank-account/account.query/src/main/java/com/techbank/account/query/infrastructure/handlers/EventHandler.java
@@ -0,0 +1,13 @@
+package com.techbank.account.query.infrastructure.handlers;
+
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawnEvent;
+
+public interface EventHandler {
+    void on(AccountOpenedEvent event);
+    void on(FundsDepositedEvent event);
+    void on(FundsWithdrawnEvent event);
+    void on(AccountClosedEvent event);
+}
